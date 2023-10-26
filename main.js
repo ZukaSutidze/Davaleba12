1b9baa6d5d478b4c9d5b652c4ef70a99648cd4f8
ZukaSutidze <zuka.sutidze@gmail.com>
2023-10-26 20:24:47 +0400

    Davaleba 12

---
+++ main.js
@@ -0,0 +1,8 @@
+const brg = document.getElementById("logo");
+const brg1 = document.querySelector('.Burgermenu'); 
+
+brg.addEventListener('click', function() {
+    brg1.classList.toggle('active'); 
+});
+
+
